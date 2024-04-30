9522824cb218d9f82d6e02c5d754ab60012726ce
DuckY-Y <Rowan@nock.nl>
2024-05-01 00:36:09 +0200

    made a ssh setup script

---
+++ setup_ssh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+source "./config_cron.sh"
+
+SSH_KEY="$HOME/.ssh/id_ed25519"
+
+if [ ! -f "$SSH_KEY" ]; then
+    ssh-keygen -t ed25519 -f "$SSH_KEY" -N ''
+fi
+
+ssh-copy-id -i "$SSH_KEY.pub" "$REMOTE_MACHINE"
