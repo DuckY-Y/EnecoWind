1dc933a8a5fcceda56a50694767c32d3c8feafe2
DuckY-Y <Rowan@nock.nl>
2024-05-01 14:24:44 +0200

    Ability to delete files after copying

--- copy_cron.sh
+++ copy_cron.sh
@@ -4,6 +4,14 @@ scp -r "$REMOTE_MACHINE":"$TARGET_DIR"/loggingdatawing_ps "$DEST_DIR"/
 scp -r "$REMOTE_MACHINE":"$TARGET_DIR"/loggingdatawing_sb "$DEST_DIR"/
 if [ $? -eq 0 ]; then
     echo "Directories copied successfully."
+    if [ "$DELETE_FILES" = "true" ]; then
+        ssh "$REMOTE_MACHINE" "rm -r \"$TARGET_DIR\"/loggingdatawing_ps \"$TARGET_DIR\"/loggingdatawing_sb"
+        if [ $? -eq 0 ]; then
+            echo "Directories deleted successfully on the target machine."
+        else
+            echo "Error: Failed to delete directories on the target machine."
+        fi
+    fi
 else
     echo "Error: Failed to copy directories."
 fi
