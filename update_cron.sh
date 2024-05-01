a1bed2600ec8980c5ae4dfe3a843652a2dba9c7b
DuckY-Y <Rowan@nock.nl>
2024-05-01 14:07:41 +0200

    modified:   update_cron.sh

--- update_cron.sh
+++ update_cron.sh
@@ -6,7 +6,7 @@ update_cron_job() {
     UPDATE_FILE="./copy_cron.sh"
     if [ -f "$UPDATE_FILE" ]; then
         # Set the cron job to run at the time specified in the configuration
-        NEW_CRON_JOB="$CRON_TIME $(cat "$UPDATE_FILE")"
+        NEW_CRON_JOB="$CRON_TIME bash $PWD/$UPDATE_FILE"
         (crontab -l ; echo "$NEW_CRON_JOB") | crontab -
         
         if [ $? -eq 0 ]; then
