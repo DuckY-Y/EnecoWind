8ce363ff9c2d08a4dd3624b40813eee5f73095f7
Rowan Tuinhout <Rowan@Nock.nl>
2024-07-11 16:05:42 +0200

    Fixed bug
    source in copy_cron.sh is missing a folder before the target config file, pwd is not working as intented

--- copy_cron.sh
+++ copy_cron.sh
@@ -1,13 +1,37 @@
 #!/bin/bash'
 
-source "./config_cron.sh"
+source "$PWD/config_cron.sh" # present working directory for some reason is missing the last folder
 
-scp -r "$REMOTE_MACHINE":"$TARGET_DIR"/loggingdatawing_ps "$DEST_DIR"/
-scp -r "$REMOTE_MACHINE":"$TARGET_DIR"/loggingdatawing_sb "$DEST_DIR"/
+echo "Remote machine: $REMOTE_MACHINE"
+echo "Target directory: $TARGET_DIR"
+echo "Destination directory: $DEST_DIR"
+
+scp -r "$REMOTE_MACHINE":"$TARGET_DIR"/loggingdatawing_aft "$DEST_DIR"/
+scp -r "$REMOTE_MACHINE":"$TARGET_DIR"/loggingdatawing_fwd "$DEST_DIR"/
 if [ $? -eq 0 ]; then
     echo "Directories copied successfully."
     if [ "$DELETE_FILES" = "true" ]; then
-        ssh "$REMOTE_MACHINE" "rm -r \"$TARGET_DIR\"/loggingdatawing_ps \"$TARGET_DIR\"/loggingdatawing_sb"
+        ssh "$REMOTE_MACHINE" "rm -r \"$TARGET_DIR\"/loggingdatawing_aft \"$TARGET_DIR\"/loggingdatawing_fwd"
+        if [ $? -eq 0 ]; then
+            echo "Directories deleted successfully on the target machine."
+        else
+            echo "Error: Failed to delete directories on the target machine."
+        fi
+    fi
+else
+    echo "Error: Failed to copy directories."
+fi
+
+#!/bin/bash
+
+source "$PWD/config_cron.sh"
+
+scp -r "$REMOTE_MACHINE":"$TARGET_DIR"/loggingdatawing_aft "$DEST_DIR"/
+scp -r "$REMOTE_MACHINE":"$TARGET_DIR"/loggingdatawing_fwd "$DEST_DIR"/
+if [ $? -eq 0 ]; then
+    echo "Directories copied successfully."
+    if [ "$DELETE_FILES" = "true" ]; then
+        ssh "$REMOTE_MACHINE" "rm -r \"$TARGET_DIR\"/loggingdatawing_aft \"$TARGET_DIR\"/loggingdatawing_fwd"
         if [ $? -eq 0 ]; then
             echo "Directories deleted successfully on the target machine."
         else
