baa4312dc2a4a6e0965a04a4e6ff7dc5b33ee50f
Rowan Tuinhout <Rowan@Nock.nl>
2024-07-11 15:23:33 +0200

    Fixed couple bugs

--- copy_cron.sh
+++ copy_cron.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+#!/bin/bash'
+
 source "./config_cron.sh"
+
 scp -r "$REMOTE_MACHINE":"$TARGET_DIR"/loggingdatawing_ps "$DEST_DIR"/
 scp -r "$REMOTE_MACHINE":"$TARGET_DIR"/loggingdatawing_sb "$DEST_DIR"/
 if [ $? -eq 0 ]; then
--- update_cron.sh
+++ update_cron.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-source "./config_cron.sh"
+source "$PWD/config_cron.sh"
 
 update_cron_job() {
     UPDATE_FILE="copy_cron.sh"
     if [ -f "$UPDATE_FILE" ]; then
         # Set the cron job to run at the time specified in the configuration
-        NEW_CRON_JOB="$CRON_TIME bash $PWD/$UPDATE_FILE"
+        NEW_CRON_JOB="$CRON_TIME bash $PWD/$UPDATE_FILE >> $PWD/copy_cron.log 2>&1"
         (crontab -l ; echo "$NEW_CRON_JOB") | crontab -
         
         if [ $? -eq 0 ]; then
